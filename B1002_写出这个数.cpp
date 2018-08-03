290944a88e09b72e4b04a6b49687adbef4138b46
dingwj <ding1392@foxmail.com>
2018-08-03 17:57:22 +0800

    3.6 字符串处理

--- B1002_写出这个数.cpp
+++ B1002_写出这个数.cpp
@@ -1,12 +1,12 @@
-// B1002 д������� 
+// B1002 写出这个数 
 #include<stdio.h>
 #include<string.h>
 /*
-	���⣺����һ����Ȼ��n���������λ����֮�͡��ú���ƴ��д���͵�ÿһλ���֣�
-	˼·��
-	    step1���� char[] ���������Ȼ�� n��ʹ�� strlen() ��� n �ĳ��ȡ�֮��ö���ַ������ÿһλ����ÿһλ��Ӧ�������ۼӡ�
-		step2���õ�ÿһλ���ۼӽ�� sum��Ϊ���㽫 sum �Ӹ�λ����λ������� sum ��ÿһλ�浽����ans[]�С� 
-		step3���ö�ά char change[][] ���飬�� 0~9 ��ӦΪ "ling" "yi" "er"..."jiu" ���Ӹ�λ����λö������ ans[]�����ÿһλ��Ӧ���ַ���
+	题意：读入一个自然数n，计算其各位数字之和。用汉语拼音写出和的每一位数字；
+	思路：
+	    step1：用 char[] 数组读入自然数 n；使用 strlen() 获得 n 的长度。之后枚举字符数组的每一位，将每一位对应的数字累加。
+		step2：得到每一位的累加结果 sum，为方便将 sum 从高位到低位输出，将 sum 的每一位存到数组ans[]中。 
+		step3：用二维 char change[][] 数组，将 0~9 对应为 "ling" "yi" "er"..."jiu" 。从高位到低位枚举数组 ans[]，输出每一位对应的字符串
 		 
 */
 int main()
@@ -15,16 +15,16 @@ int main()
 	gets(str);
 	int len = strlen(str);
 	
-	// sum �������λ��֮�ͣ� 
+	// sum 存放所有位数之和； 
 	int sum = 0;
 	
 	for( int i = 0; i < len; i++)
 	{
-		// ��ÿһλ�ۼӣ� 
+		// 将每一位累加； 
 		sum += str[i] - '0';
 	}
 	
-	// num ��ʾ sum ��λ��
+	// num 表示 sum 的位数
 	int num = 0;
 	int ans[10] = {0};
 	while( sum != 0)
@@ -33,19 +33,21 @@ int main()
 		sum /= 10;
 	} 
 	
+	// change[0] = "ling",  change[1] = "yi",  change[2] = "er",
+	// change[3] = "san",  change[4] = "si",  change[5] = "wu"......
 	char change[10][5] = {
 		"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"
 	};
 	
-	// �Ӹ�λ��ʼ��� 
+	// 从高位开始输出 
 	for( int i = num - 1; i >=0 ; i--)
 	{
-		// ans[i] ��Ϊ�Ӹ�λ��ʼ�ĵ� i ������  
+		// ans[i] 即为从高位开始的第 i 个数字  
 		printf("%s", change[ ans[i] ]);
 		
 		/*
-		   ��� i û�е� 0��(���������һ��ѭ��)��������ո�;
-		   ��������һ��ѭ�������������; 
+		   如果 i 没有到 0，(即不是最后一次循环)，则输出空格;
+		   如果是最后一次循环，则输出换行; 
 		*/
 		if( i != 0 )
 			printf(" ");
